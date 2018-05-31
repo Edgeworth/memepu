55c5d0049eeb460bd23b110b80b9c4b3470c8d09
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2018-05-31 21:34:04 +0900

    Control logic progress.

--- kicad_script.py
+++ kicad_script.py
@@ -1,6 +1,7 @@
 import subprocess
 
-in_labels = ['~A_IN_NCLK',
+in_labels = [
+'~A_IN_NCLK',
 '~B_IN_NCLK',
 '~M0_IN_NCLK',
 '~M1_IN_NCLK',
@@ -22,7 +23,20 @@ in_labels = ['~A_IN_NCLK',
 '~DISP_IN_NCLK',
 '~TASK_IN_NCLK',
 '~OPCODE0_IN_NCLK',
-'~OPCODE1_IN_NCLK']
+'~OPCODE1_IN_NCLK',
+'~INT0_IN_NCLK',
+'~INT1_IN_NCLK',
+'~INT2_IN_NCLK',
+'~INT3_IN_NCLK',
+'~INT4_IN_NCLK',
+'~INT5_IN_NCLK',
+'~INT6_IN_NCLK',
+'~INT7_IN_NCLK',
+]
+
+# Out of in_labels, when we make CLK versions, these ones should be
+# local rather than hierarchical labels.
+in_local_labels = ['OPCODE0_IN_NCLK', 'OPCODE1_IN_NCLK']
 
 out_labels = ['~A_OUT_NCLK',
 '~B_OUT_NCLK',
@@ -41,7 +55,8 @@ out_labels = ['~A_OUT_NCLK',
 '~MMU_OUT_NCLK',
 '~MMU_CONTROL_OUT_NCLK',
 '~TASK_OUT_NCLK',
-'~MLU_OUT_NCLK']
+'~MLU_OUT_NCLK',
+'~CTRLLOGIC_OUT_NCLK']
 
 ctrl_labels_nclk = ["~SUB_NCLK",
 "~RESET_NCLK",
@@ -59,6 +74,10 @@ ctrl_labels_clk = [
 "~KSP_DEC_NCLK",
 "~ALU_FLAG_SET_NCLK"]
 
+ctrl_local_lables = [
+'OPCODE0_INC_NCLK', 'OPCODE1_INC_NCLK'
+]
+
 subprocess.call(['xdotool', 'search', '--name', '/Eeschema.*/', 'windowfocus'])
 
 def chunks(l, n):
@@ -67,6 +86,9 @@ def chunks(l, n):
 def tr(s):
 	m = str.maketrans({'_': 'underscore', '~': 'asciitilde'})
 	return [c.translate(m) for c in s]
+
+def replace(l, fr, to):
+	return [i.replace(fr, to) for i in l]
 
 def run_xdotool_key(*keys):
 	cmd = ['xdotool', 'key', '--delay', '15'] + list(keys)
@@ -102,15 +124,35 @@ def nav(s):
 			res += code
 	return res
 
-def generate_clk_anders(left_labels, right_labels):
+def generate_clk_anders(left_labels, right_labels, local_labels=[]):
+	col_count = 0
 	for l in chunks(list(zip(left_labels, right_labels)), 4):
 		run_xdotool_key('l', *tr('CLK'), *nav('ErrEDD'))
 		for left, right in l:
 			run_xdotool_key('l', *tr(left), *nav('ErrECRR2U'))
-			run_xdotool_key('h', *tr(right), *nav('ErrE2DCLL2D'))
+			key = 'l' if left in local_labels else 'h'
+			run_xdotool_key(key, *tr(right), *nav('ErrE2DCLL2D'))
 		run_xdotool_key(*nav('4D'))
+		col_count += 1
 
+		if col_count == 2:
+			col_count = 0
+			run_xdotool_key(*nav('6U4CR2CU2U'))
+
+def generate_nclk_notters(labels):
+	for l in chunks(labels, 6):
+		for label in l:
+			run_xdotool_key('l', *tr(label), *nav('ErrECR2R'))
+			run_xdotool_key('l', *tr(label.replace('~', '')), *nav('EE6DCL2L'))
+		run_xdotool_key(*nav('6U4CR3CU'))
 
 # generate_labels(ctrl_labels_nclk, 'h', 2)
 # generate_labels(ctrl_labels_clk)
-generate_clk_anders(ctrl_labels_clk, [i.replace('NCLK', 'CLK') for i in ctrl_labels_clk])
+# generate_nclk_notters(in_labels)
+
+# in_labels_pos = replace(in_labels, '~', '')
+# generate_clk_anders(in_labels_pos, replace(in_labels_pos, 'NCLK', 'CLK'), in_local_labels)
+
+
+ctrl_labels_clk_pos = replace(ctrl_labels_clk, '~', '')
+generate_clk_anders(ctrl_labels_clk_pos, replace(ctrl_labels_clk_pos, 'NCLK', 'CLK'), ctrl_local_lables)
