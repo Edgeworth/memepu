ebd0643b1ec3b219f4c67a8632e0645b36ab4de2
E <edgeworth.dev@gmail.com>
2018-06-10 22:06:25 +0900

    Checkpoint.

--- kicad/script.py
+++ kicad/script.py
@@ -1,13 +1,28 @@
+
 from pcbnew import *
 
 board = GetBoard()
 netcodes = board.GetNetsByNetcode()
 
-modules = board.GetModules()
-for m in modules:
-    pos = m.GetPosition()
-    if pos[0] % 500000 != 0 or pos[1] % 500000 != 0:
-        new_pos = wxPoint(pos[0] / 500000 * 500000, pos[1] / 500000 * 500000)
-        print "Repositioning", m.GetReference(), "from", pos, "to", new_pos
-        m.SetPosition(new_pos)
-        
+def snap_modules():
+    modules = board.GetModules()
+    for m in modules:
+        pos = m.GetPosition()
+        if pos[0] % 500000 != 0 or pos[1] % 500000 != 0:
+            new_pos = wxPoint(pos[0] / 500000 * 500000, pos[1] / 500000 * 500000)
+            print "Repositioning", m.GetReference(), "from", pos, "to", new_pos
+            m.SetPosition(new_pos)
+            
+def position_labels():
+    modules = board.GetModules()
+    for m in modules:
+        ref_text = m.GetReference()
+        # print ref_text
+        if ref_text == 'U15':
+            print "FOUND U15. Rotating."
+            ref = m.Reference()
+            pos = ref.GetPosition()
+            cur_rot = ref.GetDrawRotation()
+            ref.Rotate(pos, -cur_rot + 900)
+
+position_labels()
