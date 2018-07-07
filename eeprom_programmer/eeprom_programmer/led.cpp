4ff6a950332a094e74b030b1898e5636960ab6b0
E <edgeworth.dev@gmail.com>
2018-07-07 22:54:24 +0900

    Updates.

--- eeprom_programmer/eeprom_programmer/led.cpp
+++ eeprom_programmer/eeprom_programmer/led.cpp
@@ -4,12 +4,12 @@
 namespace {
 enum {
   B = 1 << 0,
-  BR = 1 << 1,
-  DOT = 1 << 2,
-  BL = 1 << 3,
-  M = 1 << 7,
-  TL = 1 << 6,
-  T = 1 << 5,
+  BR = 1 << 6,
+  DOT = 1 << 5,
+  BL = 1 << 1,
+  M = 1 << 2,
+  TL = 1 << 7,
+  T = 1 << 3,
   TR = 1 << 4
 };
 
@@ -40,21 +40,21 @@ uint8_t buildByte(uint16_t addr, const int(&bits)[Size]) {
   uint8_t byte = 0;
   for (int bit : bits) {
     byte <<= 1;
-    byte |= (bool)(addr & (1 << bit));
+    byte |= (bool)(addr & (1uL << bit));
   }
   return byte;
 }
 
 uint8_t buildDataByte(uint16_t addr) {
-  return buildByte(addr, {1, 2, 3, 4, 5, 6, 7, 12});
+  return buildByte(addr, {7, 6, 5, 4, 3, 2, 1, 0});
 }
 
 uint8_t buildSegByte(uint16_t addr) {
-  return buildByte(addr, {0, 10});
+  return buildByte(addr, {9, 8});
 }
 
 uint8_t buildModeByte(uint16_t addr) {
-  return buildByte(addr, {9, 11});
+  return buildByte(addr, {12, 11, 10});
 }
 
 int getDigit(uint8_t val, int dig, int base) {
@@ -66,44 +66,43 @@ int getDigit(uint8_t val, int dig, int base) {
 }
 }
 
-void runLed() {
-  unsigned long start = millis();
+void runLed(bool debug) {
+  AT28C64 eeprom;
+  // Delay because hardware data protection prevents write for 5 ms on startup.
+  delay(10);
 
-  EEPROM eeprom(10);
-  delay(10);  // Delay because flake.
-
+  printf("Writing LED bytes");
   for (int i = 0; i < 8192; ++i) {
     const uint8_t data = buildDataByte(i);
     const int8_t data_signed = *reinterpret_cast<const int8_t*>(&data);
     const uint8_t seg = buildSegByte(i);
     const uint8_t mode = buildModeByte(i);
-    const bool sign = i & (1 << 8);
+    const bool sign = mode & 1;
     const int base = (mode & (1 << 1)) ? 16 : 10;
     const int place = 3 - seg;  // digits place
     const int digit = getDigit(sign ? abs(data_signed) : data, place, base);
 
-    printf("data %d, signed data %d, place: %d, base: %d, sign: %d, digit: %d\n", 
-    	int(data), int(data_signed), int(place), int(base), int(sign), int(digit));
+    if (debug) {
+      printf("data %d, signed data %d, place: %d, base: %d, sign: %d, digit: %d\n",
+    	    int(data), int(data_signed), int(place), int(base), int(sign), int(digit));
+    }
     uint8_t val = digit;
     if (digit >= 0) {
-    	printf("writing normal digit\n");
+    	if (debug) printf("writing normal digit\n");
     	val = digitToLed(digit);
     } else if (digit == -place && base == 16) {  // (h)FF
-    	printf("writing h\n");
+    	if (debug) printf("writing h\n");
     	val = TL | BL | M | BR;
     } else if (digit == -place + 1 && base == 16 && sign && data_signed < 0) {  // (-)hAF
-    	printf("writing neg sign (base 16)\n");
+    	if (debug) printf("writing neg sign (base 16)\n");
     	val = M;
-    } else if (digit == -place && base == 10 && sign && data_signed < 0) {  // (-)10 
-    	printf("writing neg sign (base 10)\n");
+    } else if (digit == -place && base == 10 && sign && data_signed < 0) {  // (-)10
+    	if (debug) printf("writing neg sign (base 10)\n");
     	val = M;
     } else {
-    	printf("writing no dig\n");
+    	if (debug) printf("writing no dig\n");
     	val = 0;
     }
-    eeprom.write(i, &val, 1);
+    eeprom.writeByte(i, val);
   }
-
-  printf("Written in %ldms\n", millis() - start);
-  eeprom.print(0, 8192);
 }
