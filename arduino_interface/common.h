53cb521b4d55551648dd9b5d6a67315f9b71da8c
Eliot Courtney <edgeworth.dev@gmail.com>
2018-07-22 19:49:05 +0900

    Add offsets specifiable in the assembler.

---
+++ arduino_interface/common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+#include "Arduino.h"
+
+extern char printf_buf[128];
+extern int selection;
+
+void printTime(unsigned long us);
+
+#define printf(...) do { \
+  snprintf(printf_buf, sizeof(printf_buf), __VA_ARGS__); \
+  Serial.print(printf_buf); \
+} while (0)
+
+#define PROFILE(x) do { \
+  unsigned long st = micros(); \
+  x; \
+  unsigned long en = micros(); \
+  printf("Time for " #x ": "); \
+  printTime(en - st); \
+  printf("\n"); \
+} while(0)
+
+#define CHECK(x) do { \
+  if (!(x)) { \
+    printf("Assertion " #x " failed."); \
+  } \
+} while (0)
+
+#endif
--- memepu/src/programs/assembler.cpp
+++ memepu/src/programs/assembler.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[]) {
   verify_expr(argc == 2, "need filename");
   std::string data = readFile(argv[1]);
-  Asm assembler;
-  writeFile("rom.bin", assembler.assembleToBinaryData(data));
+  Asm assembler(data);
+  writeFile("rom.bin", assembler.assembleToBinaryData());
 }
