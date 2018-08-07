8931ec719867696662f61b56e52447b8fd376f93
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2018-08-07 12:00:18 +0900

    Interrupts fully working

--- scripts/interrupt.asm
+++ scripts/interrupt.asm
@@ -1,31 +1,27 @@
-LDA 1
-LDB 1
-EINT ;Enable interrupts.
-start0:
-  DSP
-  STA 2048
-  LDAADDR 2048
-  DSP
-  ADD
-  JMP start0
 LDA 0
 LDB 1
+EINT ;Enable interrupts.
 start: ;Compute fibonnaci
   ADD
   DSP
   SWP
   JMP start
-
 ;Interrupt handler
 040000:
-  LDAADDR 0
+  IN0
+  CMP ;0 is dump, 1 is read. Compare A to 0.
+  JZH dump
+  LDA 1
   DSP
-  LDAADDR 1
+  OUT0 75 ;Send bytes
+  OUT0 69
+  OUT0 75
+  OUT0 10
+  OUT0 0
+  JMP return
+dump:
+  LDA 0
   DSP
-  LDAADDR 2
-  DSP
-  LDAADDR 3
-  DSP
-  LDAADDR 4
-  DSP
+  DMP ;Dump internal state
+return:
   RFI ;Return from interrupt.
