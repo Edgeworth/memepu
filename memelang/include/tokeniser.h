4b4d643ae1173bdaf35bd5fa9f17c26f8c60540f
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2018-09-01 23:19:28 +0900

    Progress on parser.

--- memelang/include/tokeniser.h
+++ memelang/include/tokeniser.h
@@ -1,19 +1,27 @@
+#include <utility>
+
+
 #ifndef MEMELANG_TOKENISER_H
 #define MEMELANG_TOKENISER_H
 
 #include "common.h"
+#include "file_contents.h"
 #include <vector>
+#include <utility>
+#include <memory>
 
 struct Token {
   enum Type {
     INTERFACE,
     STRUCT,
+    FUNCTION,
     PLUS,
     MINUS,
     ASTERISK,
     FSLASH,
     QUOTE,
     FOR,
+    WHILE,
     LPAREN,
     RPAREN,
     LBRACE,
@@ -28,23 +36,27 @@ struct Token {
     COMMA,
     COLON
   } type;
-  std::string data;
-  int linenum;
-  int colnum;
+  int loc;
+  int size;
+
+  std::string toString(const FileContents* contents) const {
+    std::string str;
+    format_str(str, "Token('%s', %d:%d)", contents->getSpan(loc, size).c_str(), contents->getLineNumber(loc), contents->getColNumber(loc));
+    return str;
+  }
 };
 
 class Tokeniser {
 public:
-  explicit Tokeniser(const std::string& data) : data_(data) {}
+  explicit Tokeniser(const FileContents* contents) : contents_(contents) {}
 
   std::vector<Token> tokenise();
 
 private:
-  std::string data_;
+  const FileContents* contents_;
   std::string curtok_;
   std::vector<Token> tokens_;
-  int linenum_ = 1;
-  int colnum_ = 1;
+  int idx_ = 0;
 
   void pushCurrentToken();
   bool startsNewToken(char c);
