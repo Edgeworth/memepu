271fa4c9a04a67cef4c7edc508079bce6af8a072
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2018-09-16 23:27:53 +0900

    Work on moving everything to 32 bit + proper paging + memory mapped IO.

--- memelang/src/llvm.cpp
+++ memelang/src/llvm.cpp
@@ -13,7 +13,15 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Verifier.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Transforms/Utils/Cloning.h"
 
 namespace meme {
 
@@ -22,6 +30,21 @@ LlvmCodegen::LlvmCodegen(FileContents* contents, const Compile& compile) : conte
   typemap_[INT16] = llvm::Type::getInt16Ty(ctx_);
   typemap_[UINT8] = llvm::Type::getInt8Ty(ctx_);
   typemap_[UINT16] = llvm::Type::getInt16Ty(ctx_);
+
+  LLVMInitializeX86TargetInfo();
+  LLVMInitializeX86Target();
+  LLVMInitializeX86TargetMC();
+  LLVMInitializeX86AsmParser();
+  LLVMInitializeX86AsmPrinter();
+  auto triple = llvm::sys::getDefaultTargetTriple();
+  std::string err;
+  auto* target = llvm::TargetRegistry::lookupTarget(triple, err);
+  verify_expr(target, "error with llvm target %s: %s", triple.c_str(), err.c_str());
+  machine_ = target->createTargetMachine(triple, "generic", "", llvm::TargetOptions(), llvm::Optional<llvm::Reloc::Model>());
+  module_.setDataLayout(machine_->createDataLayout());
+  module_.setTargetTriple(triple);
+
+  machine_->Options.MCOptions.AsmVerbose = true;
 }
 
 
@@ -31,6 +54,9 @@ void LlvmCodegen::codegen() {
     const auto& func = iter.second;
     generateFunction(func);
   }
+
+  outputLlvmFile("a.asm", llvm::TargetMachine::CGFT_AssemblyFile);
+  outputLlvmFile("a.o", llvm::TargetMachine::CGFT_ObjectFile);
 }
 
 llvm::Function *LlvmCodegen::generateFunction(const Func& func) {
@@ -45,7 +71,8 @@ llvm::Function *LlvmCodegen::generateFunction(const Func& func) {
 
   auto* block = llvm::BasicBlock::Create(ctx_, "entry", llvm_func);
   builder_.SetInsertPoint(block);  // Start writing stuff into the block.
-  builder_.CreateRet(llvm::ConstantInt::get(ctx_, llvm::APInt(8, 42, false)));  // TODO: test uint8 = 42
+
+  generateBlock(func.defn->children.at(0).get());
 
   llvm::verifyFunction(*llvm_func);
   fprintf(stderr, "Made function: ");
@@ -54,10 +81,53 @@ llvm::Function *LlvmCodegen::generateFunction(const Func& func) {
   return llvm_func;
 }
 
+void LlvmCodegen::generateBlock(const Parser::Node* node) {
+  for (const auto& child : node->children) {
+    generateStatement(child.get());
+  }
+}
+
+void LlvmCodegen::generateStatement(const Parser::Node* node) {
+  switch (node->type) {
+    default:
+      generateExpression(node);
+  }
+}
+
+llvm::Value* LlvmCodegen::generateExpression(const Parser::Node* node) {
+  std::vector<llvm::Value*> values;
+  for (const auto& child : node->children)
+    values.push_back(generateExpression(child.get()));
+
+  fprintf(stderr, "")
+
+  switch (node->type) {
+    case Parser::Node::ADD:
+      return builder_.CreateAdd(values.at(0), values.at(1), "add");
+    case Parser::Node::INTEGER_LITERAL:
+      return llvm::ConstantInt::get(ctx_, llvm::APInt(8, uint64_t(node->intdata), false));
+    default:
+      verify_expr(false, "should never happen, node of type: %d", int(node->type));
+  }
+}
+
 llvm::Type* LlvmCodegen::getLlvmType(const Type& type) {
   auto iter = typemap_.find(type);
   verify_expr(iter != typemap_.end(), "unknown type");
   return iter->second;
+}
+
+void LlvmCodegen::outputLlvmFile(const std::string& name, llvm::TargetMachine::CodeGenFileType type) {
+  std::error_code error_code;
+  llvm::raw_fd_ostream file(name, error_code, llvm::sys::fs::F_None);
+  verify_expr(!error_code, "could not open llvm file for writing: %s", error_code.message().c_str());
+
+  llvm::legacy::PassManager pass;
+  verify_expr(!machine_->addPassesToEmitFile(pass, file, type),  "could not write llvm file");
+
+  auto module = llvm::CloneModule(&module_);
+  pass.run(*module);
+  file.flush();
 }
 
 }  // namespace meme
