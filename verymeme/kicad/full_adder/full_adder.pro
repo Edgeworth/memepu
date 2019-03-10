0919454d4ad7cdbb19cd48baca8c253824e804fb
Eliot Courtney <edgeworth.dev@gmail.com>
2019-03-10 18:37:19 +0900

    About to refactor kicad parsing.

--- verymeme/kicad/full_adder/full_adder.pro
+++
@@ -1,4 +0,0 @@
-update=2019年02月09日 18時08分41秒
-last_client=kicad
-[general]
-version=1
--- verymeme/src/programs/yosys_test.cpp
+++ verymeme/src/programs/yosys_test.cpp
@@ -22,21 +22,22 @@ public:
           static_cast<int>(sigmap.database.size()));
       for (auto& cell_iter : module->cells_) {
         const auto&[cell_id, cell] = cell_iter;
-        log("\n\nChecking cell %s\n", log_id(cell_id));
+        log("  Checking cell %s, name: %s, type: %s\n", log_id(cell_id),
+            log_id(cell->name), log_id(cell->type));
         for (auto& conn : cell->connections()) {
           const auto& conn_id = conn.first;
           const auto& sig = sigmap(conn.second);
-          log("Looking at connection: %s, signal: %s, signal size: %d\n", log_id(conn_id),
+          log("    Looking at connection: %s, signal: %s, signal size: %d\n", log_id(conn_id),
               log_signal(sig), sig.size());
         }
       }
 
-      log("Looking at wires\n");
+      log("  Looking at wires\n");
       for (auto& wire_iter : module->wires_) {
         const auto&[wire_id, wire] = wire_iter;
-        log("Looking at wire %s\n", log_id(wire_id));
-
+        log("    Looking at wire %s\n", log_id(wire_id));
       }
+      printf("\n");
     }
   }
 } test_pass;
