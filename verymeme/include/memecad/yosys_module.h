0ab56861323a7ce0c6247c539bbf666188409cab
Eliot Courtney <edgeworth.dev@gmail.com>
2019-03-27 22:30:14 +0900

    Add schematic output test.

--- verymeme/include/memecad/yosys_module.h
+++ verymeme/include/memecad/yosys_module.h
@@ -8,11 +8,17 @@
 
 namespace memecad {
 
+std::vector<Schematic::SchematicFile> convertVerilogToKicadSchematics(
+    const std::string& memecad_map_filename, const std::vector<std::string>& verilog_filenames,
+    const std::vector<std::string>& kicad_library_filenames);
+
 class TestPass : public Yosys::Pass {
 public:
   TestPass(const std::string& memecad_map_filename, const std::string& kicad_library_filename);
 
   void execute(std::vector<std::string> args, Yosys::RTLIL::Design* design) override;
+  Mapper& getMapper() { return mapper_; }
+
 private:
   Mapper mapper_;
 
---
+++ verymeme/test_data/common/chip7432.v
@@ -0,0 +1,7 @@
+module chip7432(
+  input wire [3:0] A,
+  input wire [3:0] B,
+  output wire [3:0] Y
+);
+  assign Y = A | B;
+endmodule
