88cb415fcb962dc23e8de57c14fc9b74c75f14e6
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2019-03-29 00:03:41 +0900

    Implement no-connect via verilog signals whose name starts with 'unused'.

--- verymeme/test_data/hierarchical0/tapb_top_level.v
+++ verymeme/test_data/hierarchical0/tapb_top_level.v
@@ -1,10 +1,8 @@
-module tapb_top_level(
-  input wire [3:0] LOW_B
-);
-  logic [7:0] out;
-  logic [3:0] HIGH_B;
+module tapb_top_level();
+  logic [7:0] unused_out;
+  logic [3:0] high_b;
 
-  assign HIGH_B = 4'b1010;
+  assign high_b = 4'b1010;
 
-  two_a_plus_b tapb(.A(8'd42), .B({HIGH_B, LOW_B}), .OUT(out));
+  two_a_plus_b tapb(.A(8'd42), .B({high_b, 4'b1010}), .OUT(unused_out));
 endmodule
