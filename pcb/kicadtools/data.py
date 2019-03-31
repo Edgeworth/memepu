aa8495e0b234a6b2c0b1bea179944e91fcc887d1
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2019-04-01 03:04:09 +0900

    Adding 74LVC162244 symbol.

--- pcb/kicadtools/data.py
+++ pcb/kicadtools/data.py
@@ -1,6 +1,16 @@
+# Symbol data format:
+#   group_name: <array>|<dict>
+#     |group_name| is case insensitive (converted to upper-case)
+#     If the value is an array of integers, pins named by the group name and
+#     each integer are created. If the array length is 1, just the group name is
+#     used. If a dictionary of pin name to number is given, those pin names are
+#     used.
+#   num_pins: Indicates the total number of pins in the symbol, for error checking.
 data_71V632_symbol = {
-    'io': [52, 53, 56, 57, 58, 59, 62, 63, 68, 69, 72, 73, 74,
-           75, 78, 79, 2, 3, 6, 7, 8, 9, 12, 13, 18, 19, 22, 23, 24, 25, 28, 29],
+    'io': [
+        52, 53, 56, 57, 58, 59, 62, 63, 68, 69, 72, 73, 74, 75, 78, 79, 2, 3, 6,
+        7, 8, 9, 12, 13, 18, 19, 22, 23, 24, 25, 28, 29
+    ],
     'vcc': [4, 11, 15, 20, 27, 41, 54, 61, 65, 70, 77, 91],
     'gnd': [5, 10, 17, 21, 26, 40, 55, 60, 67, 71, 76, 90],
     'addr': [37, 36, 35, 34, 33, 32, 100, 99, 82, 81, 44, 45, 46, 47, 48, 49],
@@ -22,7 +32,8 @@ data_71V632_symbol = {
         '~bw4': 96,
     },
     'nc': [1, 14, 16, 30, 38, 39, 42, 43, 50, 51, 64, 66, 80],
-    'num_pins': 100
+    'num_pins':
+        100
 }
 
 data_GTL2000_symbol = {
@@ -32,28 +43,47 @@ data_GTL2000_symbol = {
         'DREF': 67
     },
     'gnd': [1],
-    'D': [46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25],
-    'S': [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-    'num_pins': 48
+    'D': [
+        46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29,
+        28, 27, 26, 25
+    ],
+    'S': [
+        3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+        23, 24
+    ],
+    'num_pins':
+        48
 }
 
+data_74LVC162244_symbol = {'num_pins': 48}
 
 # Check:  Pins 69, 83, 120 used by 4 rank DDR2 SO-DIMMs.
+# BA2 is NC for 256 MB and 512 MB
 data_DDR2_labels = {
-    'left': ['VREF', 'GND', 'DQ0', 'DQ1', 'GND', '~DQS0', 'DQS0', 'GND', 'DQ2', 'DQ3', 'GND', 'DQ8', 'DQ9', 'GND',
-             '~DQS1', 'DQS1', 'GND', 'DQ10', 'DQ11', 'GND', 'GND', 'DQ16', 'DQ17', 'GND', '~DQS2', 'DQS2', 'GND',
-             'DQ18', 'DQ19', 'GND', 'DQ24', 'DQ25', 'GND', 'DM3', 'NC', 'GND', 'DQ26', 'DQ27', 'GND', 'CKE0', 'VDD',
-             # BA2 is NC for 256 MB and 512 MB
-             'NC', 'BA2', 'VDD', 'A12', 'A9', 'A8', 'VDD', 'A5', 'A3', 'A1', 'VDD', 'A10/AP', 'BA0', '~WE', 'VDD',
-             '~CAS', '~S1', 'VDD', 'ODT1', 'GND', 'DQ32', 'DQ33', 'GND', '~DQS4', 'DQS4', 'GND', 'DQ34', 'DQ35', 'GND',
-             'DQ40', 'DQ41', 'GND', 'DM5', 'GND', 'DQ42', 'DQ43', 'GND', 'DQ48', 'DQ49', 'GND', 'NC', 'GND', '~DQS6',
-             'DQS6', 'GND', 'DQ50', 'DQ51', 'GND', 'DQ56', 'DQ57', 'GND', 'DM7', 'GND', 'DQ58', 'DQ59', 'GND', 'SDA',
-             'SCL', 'VDDSPD'],
-    'right': ['GND', 'DQ4', 'DQ5', 'GND', 'DM0', 'GND', 'DQ6', 'DQ7', 'GND', 'DQ12', 'DQ13', 'GND', 'DM1', 'GND', 'CK0',
-              '~CK0', 'GND', 'DQ14', 'DQ15', 'GND', 'GND', 'DQ20', 'DQ21', 'GND', 'NC', 'DM2', 'GND', 'DQ22', 'DQ23',
-              'GND', 'DQ28', 'DQ29', 'GND', '~DQS3', 'DQS3', 'GND', 'DQ30', 'DQ31', 'GND', 'CKE1', 'VDD', 'NC', 'NC',
-              'VDD', 'A11', 'A7', 'A6', 'VDD', 'A4', 'A2', 'A0', 'VDD', 'BA1', '~RAS', '~S0', 'VDD', 'ODT0', 'A13',
-              'VDD', 'NC', 'GND', 'DQ36', 'DQ37', 'GND', 'DM4', 'GND', 'DQ38', 'DQ39', 'GND', 'DQ44', 'DQ45', 'GND',
-              '~DQS5', 'DQS5', 'GND', 'DQ46', 'DQ47', 'GND', 'DQ52', 'DQ53', 'GND', 'CK1', '~CK1', 'GND', 'DM6', 'GND',
-              'DQ54', 'DQ55', 'GND', 'DQ60', 'DQ61', 'GND', '~DQS7', 'DQS7', 'GND', 'DQ62', 'DQ63', 'GND', 'SA0', 'SA1']
+    'left': [
+        'VREF', 'GND', 'DQ0', 'DQ1', 'GND', '~DQS0', 'DQS0', 'GND', 'DQ2',
+        'DQ3', 'GND', 'DQ8', 'DQ9', 'GND', '~DQS1', 'DQS1', 'GND', 'DQ10',
+        'DQ11', 'GND', 'GND', 'DQ16', 'DQ17', 'GND', '~DQS2', 'DQS2', 'GND',
+        'DQ18', 'DQ19', 'GND', 'DQ24', 'DQ25', 'GND', 'DM3', 'NC', 'GND',
+        'DQ26', 'DQ27', 'GND', 'CKE0', 'VDD', 'NC', 'BA2', 'VDD', 'A12', 'A9',
+        'A8', 'VDD', 'A5', 'A3', 'A1', 'VDD', 'A10/AP', 'BA0', '~WE', 'VDD',
+        '~CAS', '~S1', 'VDD', 'ODT1', 'GND', 'DQ32', 'DQ33', 'GND', '~DQS4',
+        'DQS4', 'GND', 'DQ34', 'DQ35', 'GND', 'DQ40', 'DQ41', 'GND', 'DM5',
+        'GND', 'DQ42', 'DQ43', 'GND', 'DQ48', 'DQ49', 'GND', 'NC', 'GND',
+        '~DQS6', 'DQS6', 'GND', 'DQ50', 'DQ51', 'GND', 'DQ56', 'DQ57', 'GND',
+        'DM7', 'GND', 'DQ58', 'DQ59', 'GND', 'SDA', 'SCL', 'VDDSPD'
+    ],
+    'right': [
+        'GND', 'DQ4', 'DQ5', 'GND', 'DM0', 'GND', 'DQ6', 'DQ7', 'GND', 'DQ12',
+        'DQ13', 'GND', 'DM1', 'GND', 'CK0', '~CK0', 'GND', 'DQ14', 'DQ15',
+        'GND', 'GND', 'DQ20', 'DQ21', 'GND', 'NC', 'DM2', 'GND', 'DQ22', 'DQ23',
+        'GND', 'DQ28', 'DQ29', 'GND', '~DQS3', 'DQS3', 'GND', 'DQ30', 'DQ31',
+        'GND', 'CKE1', 'VDD', 'NC', 'NC', 'VDD', 'A11', 'A7', 'A6', 'VDD', 'A4',
+        'A2', 'A0', 'VDD', 'BA1', '~RAS', '~S0', 'VDD', 'ODT0', 'A13', 'VDD',
+        'NC', 'GND', 'DQ36', 'DQ37', 'GND', 'DM4', 'GND', 'DQ38', 'DQ39', 'GND',
+        'DQ44', 'DQ45', 'GND', '~DQS5', 'DQS5', 'GND', 'DQ46', 'DQ47', 'GND',
+        'DQ52', 'DQ53', 'GND', 'CK1', '~CK1', 'GND', 'DM6', 'GND', 'DQ54',
+        'DQ55', 'GND', 'DQ60', 'DQ61', 'GND', '~DQS7', 'DQS7', 'GND', 'DQ62',
+        'DQ63', 'GND', 'SA0', 'SA1'
+    ]
 }
--- pcb/kicadtools/symbol.py
+++ pcb/kicadtools/symbol.py
@@ -4,17 +4,18 @@ from common import run_xdotool_key, nav, tr, listify
 
 
 def validate_data(data):
-    num_pins = data['num_pins']
-    pins = listify(data.get('nc', []))
-    for group in data.keys():
-        if group == 'num_pins' or group == 'nc': continue
-        group_data = data.get(group, {})
-        if isinstance(group_data, dict):
-            for k, v in group_data.items():
-                pins.extend(listify(v))
-        else:
-            pins.extend(listify(group_data))
-    assert len(pins) == num_pins and len(set(pins)) == num_pins
+  num_pins = data['num_pins']
+  pins = listify(data.get('nc', []))
+  for group in data.keys():
+    if group == 'num_pins' or group == 'nc':
+      continue
+    group_data = data.get(group, {})
+    if isinstance(group_data, dict):
+      for k, v in group_data.items():
+        pins.extend(listify(v))
+    else:
+      pins.extend(listify(group_data))
+  assert len(pins) == num_pins and len(set(pins)) == num_pins
 
 
 PIN_TYPES = {
@@ -34,32 +35,50 @@ PIN_DIR = {
 
 
 def reset_pin_dialog():
+  run_xdotool_key(*nav('p'))
+  time.sleep(0.05)  # wait a bit for the dialog to appear
+  run_xdotool_key('Tab', 'Tab', *nav('10U'), *(['Tab'] * 4), *nav('4U'), 'Tab',
+                  *tr('2.54'), *nav('E'), 'Escape')
+
+
+def insert_pins(prefix, numbers, pin_type, side):
+  """
+    Inserts pins of a certain type on a given side.
+
+    :param prefix: Base name of a pin, e.g. A5 => A
+    :param numbers: Pin numbers to add
+    :param pin_type: Pin type
+    :param side: Pin side (up, left, right, down)
+    :return:
+    """
+  prefix = prefix.upper()
+  for i, pin_number in enumerate(numbers):
+    pin_name = prefix
+    if len(numbers) > 1:
+      pin_name += str(i)
     run_xdotool_key(*nav('p'))
     time.sleep(0.05)  # wait a bit for the dialog to appear
-    run_xdotool_key('Tab', 'Tab', *nav('10U'), *(['Tab'] * 4), *nav('4U'), 'Tab',
-                    *tr('2.54'), *nav('E'), 'Escape')
+    run_xdotool_key(*tr(pin_name), 'Tab', *tr(pin_number), 'Tab')
+    if i == 0:  # Set up pin properties.
+      run_xdotool_key(*(['Down'] * PIN_TYPES[pin_type]), *(['Tab'] * 4),
+                      *(['Down'] * PIN_DIR[side][0]))
+    run_xdotool_key('Tab', 'Tab', *nav('EE' + PIN_DIR[side][1] * 2))
 
 
-def insert_pins(title, numbers, pin_type, side):
-    title = title.upper()
-    for i, pin_number in enumerate(numbers):
-        pin_name = title
-        if len(numbers) > 1: pin_name += str(i)
-        run_xdotool_key(*nav('p'))
-        time.sleep(0.05)  # wait a bit for the dialog to appear
-        run_xdotool_key(*tr(pin_name), 'Tab', *tr(pin_number), 'Tab')
-        if i == 0:  # Set up pin properties.
-            run_xdotool_key(
-                *(['Down'] * PIN_TYPES[pin_type]), *(['Tab'] * 4),
-                *(['Down'] * PIN_DIR[side][0]))
-        run_xdotool_key('Tab', 'Tab', *nav('EE' + PIN_DIR[side][1] * 2))
+def insert_side(data, group, pin_type, side):
+  """
+    Runs the commands for inserting some pins into the Kicad schematic editor.
 
-
-def insert_side(data, group, side, pin_type):
+    :param data:
+    :param group:
+    :param pin_type: Pin type
+    :param side: Pin side (up, left, right, down)
+    :return:
+    """
+  reset_pin_dialog()
+  group_data = data[group]
+  if isinstance(group_data, list):
+    group_data = {group: group_data}
+  for k, v in group_data.items():
+    insert_pins(k, listify(v), pin_type, side)
     reset_pin_dialog()
-    group_data = data[group]
-    if isinstance(group_data, list):
-        group_data = {group: group_data}
-    for k, v in group_data.items():
-        insert_pins(k, listify(v), pin_type, side)
-        reset_pin_dialog()
