0fa81161f5476577b8cf728ef7c06941c41d8704
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2019-04-05 22:08:09 +0900

    Add register32

--- verymeme/verilog/kpu.v
+++ verymeme/verilog/kpu.v
@@ -1,12 +1,17 @@
 `include "common.v"
+/* verilator lint_off UNUSED */  // TODO REMOVE
 module kpu(
   input wire CLK,
-  input wire N_CLK,
+//  input wire N_CLK,
   input wire N_RST
 );
+  wire [31:0] bus;
+  // Scratch registers - invisible to running code.
+  register32 tmp0(.CLK(CLK), .IN(bus), .N_OE(0), .OUT(bus));
+
   // Timer:
   wire [31:0] time32;
   timer timer(.CLK(CLK), .N_RST(N_RST), .TIME(time32));
 
-  // ALU:
+  // MLU:
 endmodule
