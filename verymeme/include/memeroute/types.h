b01a94161fd34dc30f37fe84879a94ec7b0861a5
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2019-04-13 13:03:36 +0900

    Factor parser stuff out.

---
+++ verymeme/include/memeroute/types.h
@@ -0,0 +1,14 @@
+#ifndef VERYMEME_MEMEROUTE_TYPES_H
+#define VERYMEME_MEMEROUTE_TYPES_H
+
+#include "verymeme/common.h"
+
+namespace memeroute {
+
+struct Pcb {
+  std::string name;
+};
+
+}  // memeroute
+
+#endif  // VERYMEME_MEMEROUTE_TYPES_H
