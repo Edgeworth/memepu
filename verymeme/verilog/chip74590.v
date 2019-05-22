ea9a4815674de889db24f98e8bc23dfe54942f8c
Eliot Courtney <edgeworth.dev@gmail.com>
2019-05-22 20:09:14 +0900

    Simplify 74590

--- verymeme/verilog/chip74590.v
+++ verymeme/verilog/chip74590.v
@@ -3,8 +3,6 @@ module chip74590(
   input wire N_MRC,  // Async reset
   input wire N_OE,
   input wire CPC,  // Counter clock input
-  input wire CPR,  // Register clock input
-  input wire [7:0] Q_IN,
   output logic N_RCO,  // Ripple carry output.
   output logic [7:0] Q_OUT
 );
@@ -12,9 +10,8 @@ module chip74590(
   assign Q_OUT = !N_OE ? count:8'bZ;
   assign N_RCO = count == 8'hFF ? 0 : 1;
 
-  always_ff @(posedge CPR or posedge CPC or negedge N_MRC) begin
+  always_ff @(posedge CPC or negedge N_MRC) begin
     if (!N_MRC) count <= 0;
-    else if (CPR) count <= Q_IN;
     else count <= count + 8'b1;
   end
 
@@ -28,7 +25,6 @@ module chip74590(
 
     if (f_past >= 2 && N_MRC) begin
       // If there was a positive edge to load the register, check it loaded properly.
-      if ($past(CPR) && $past(CPR) != $past(CPR, 2)) assert ($past(count) == $past(Q_IN));
       if ($past(CPC) && $past(CPC) != $past(CPC, 2)) begin
         assert ($past(count) == $past(count, 2) + 8'b1);
       end
@@ -36,16 +32,9 @@ module chip74590(
   end
 
   always_comb begin
+    if (!N_OE) assert (Q_OUT == count);
     if (!N_MRC) assert (Q_OUT == 0); // Asynchronous reset should always make it zero.
     assert (N_RCO == (count != 8'hFF));  // RCO is 1 when count isn't at edge.
-  end
-
-  always_comb begin
-    // As a compromise to deal with multiple asynchronous signals (although they are really
-    // synchronous), don't let CPR and CPC both be true at the same time, so we can distinguish them
-    // on posedge.
-    `CONTRACT(!(CPR && CPC));
-    if (!N_OE) assert (Q_OUT == count);
   end
   `endif
 endmodule
