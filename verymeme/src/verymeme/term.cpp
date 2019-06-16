8b8b3957c991dac4b4aacb0bfe8bc8e49a9fe500
E <edgeworth.dev@gmail.com>
2019-06-16 19:17:15 +0900

    Finish implementing fetch instruction. Improve memesim.

---
+++ verymeme/src/verymeme/term.cpp
@@ -0,0 +1,7 @@
+#include "verymeme/term.h"
+
+std::string
+convertToColor(const std::string& s, TermColor foreground, TermColor background, TermStyle style) {
+  return "\033[" + std::to_string(int(style)) + ";" + std::to_string(int(foreground)) + ";" +
+         std::to_string(int(background)) + "m" + s + "\033[0m";
+}
