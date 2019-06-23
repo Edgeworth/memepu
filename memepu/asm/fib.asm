a0e5e53f1a4eada80e47f97fb4a694ba4fda6127
Eliot Courtney <edgeworth.dev@gmail.com>
2019-06-23 21:09:46 +0900

    Remove memepu from toplevel directory. Superseded by verymeme.

--- memepu/asm/fib.asm
+++
@@ -1,7 +0,0 @@
-LDA 0
-LDB 1
-start:
-  ADD
-  DSP
-  SWP
-  JMP start
--- memepu/src/programs/assembler.cpp
+++
@@ -1,8 +0,0 @@
-#include "asm.h"
-
-int main(int argc, char* argv[]) {
-  verify_expr(argc == 2, "need filename");
-  std::string data = readFile(argv[1]);
-  Asm assembler(data);
-  writeFile("rom.bin", assembler.assembleToBinaryData());
-}
