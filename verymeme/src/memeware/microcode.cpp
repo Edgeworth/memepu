6d8742f7158524c0680f11764ce28de46255b1eb
E <edgeworth.dev@gmail.com>
2019-07-02 11:09:00 +0900

    Add condition variable.

--- verymeme/src/memeware/microcode.cpp
+++ verymeme/src/memeware/microcode.cpp
@@ -17,7 +17,7 @@ namespace pt = boost::property_tree;
 std::vector<uint8_t> generateMluSliceFirmware() {
   Vmlu_slice slice;
   std::vector<uint8_t> firmware;
-  const int MAX_ADDR = (1u << 12u);
+  const int MAX_ADDR = (1u << MLU_SLICE_DEPTH);
   for (int addr = 0; addr < MAX_ADDR; ++addr) {
     slice.ADDR = addr;
     slice.eval();
@@ -29,7 +29,7 @@ std::vector<uint8_t> generateMluSliceFirmware() {
 std::vector<uint8_t> generateMluLookaheadFirmware() {
   Vmlu_lookahead lookahead;
   std::vector<uint8_t> firmware;
-  const int MAX_ADDR = (1u << 17u);
+  const int MAX_ADDR = (1u << MLU_LOOKAHEAD_DEPTH);
   for (int addr = 0; addr < MAX_ADDR; ++addr) {
     lookahead.ADDR = addr;
     lookahead.eval();
@@ -41,7 +41,7 @@ std::vector<uint8_t> generateMluLookaheadFirmware() {
 std::vector<uint32_t> generateMicrocodeFirmware() {
   Vmicrocode microcode;
   std::vector<uint32_t> firmware;
-  const int MAX_ADDR = (1u << 10u);
+  const int MAX_ADDR = (1u << MICROCODE_DEPTH);
   for (int addr = 0; addr < MAX_ADDR; ++addr) {
     microcode.ADDR = addr;
     microcode.eval();
