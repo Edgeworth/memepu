8447285488f425accd11eb5e1c1c38315ffad127
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2019-09-15 12:19:04 +0900

    First memelang programme running.

--- verymeme/include/memelang/interpreter.h
+++ verymeme/include/memelang/interpreter.h
@@ -2,6 +2,7 @@
 #define MEMELANG_INTERPRETER_H
 
 #include "memelang/ast.h"
+
 namespace memelang {
 
 class Interpreter {
@@ -11,16 +12,24 @@ public:
   void run();
 
 private:
+  struct Var {
+    int64_t int_val;
+    Type* type;
+  };
 
   File* f_;
   const FileContents* cts_;
   std::unordered_map<std::string, Fn*> fns_;
-  std::unordered_map<std::string, void*> sym_; // TODO
+  std::unordered_map<std::string, std::unique_ptr<Var>> sym_; // TODO
 
   void runFn(Fn* fn);
   void runStmt(Node* stmt);
   void runVar(VarDefn* var);
   void runOp(Op* op);
+
+  Var* getVar(Node* n, const std::string& name) const;
+
+  void error(Node* n, const std::string& msg) const;
 };
 
 }  // namespace memelang
--- verymeme/src/memelang/interpreter.cpp
+++ verymeme/src/memelang/interpreter.cpp
@@ -1,19 +1,20 @@
 #include "memelang/interpreter.h"
 
+#include <memory>
 #include "verymeme/util.h"
 
 namespace memelang {
 
 namespace {
 
-template <typename T>
-T* g(Node* n) {
+template <typename T, typename R>
+T* g(R* n) {
   verify_expr(typeid(*n) == typeid(T), "BUG");
   return reinterpret_cast<T*>(n);
 }
 
-template <typename T>
-T* g(std::unique_ptr<Node>& n) {
+template <typename T, typename R>
+T* g(std::unique_ptr<R>& n) {
   return g<T>(n.get());
 }
 
@@ -23,6 +24,7 @@ Interpreter::Interpreter(memelang::File* file, const memelang::FileContents* cts
     : f_(file), cts_(cts) {}
 
 void Interpreter::run() {
+  printf("===BEGIN PROGRAM===");
   for (auto& fn : f_->fns) {
     // TODO: typelist / fn selection
     fns_[fn->sig->tname->name] = fn.get();
@@ -45,18 +47,40 @@ void Interpreter::runStmt(Node* stmt) {
     runOp(g<Op>(stmt));
   }
 }
-void Interpreter::runVar(VarDefn* var) { printf("In var: %s\n", var->decl->ref->name.c_str()); }
+void Interpreter::runVar(VarDefn* var) {
+  const auto& name = var->decl->ref->name;
+  printf("In var: %s\n", var->decl->ref->name.c_str());
+  if (sym_.count(name)) error(var, "redeclaration of var " + name);
+  sym_[name] = std::make_unique<Var>(Var{g<IntLit>(var->defn)->val, g<Type>(var->decl->type)});
+}
 
 void Interpreter::runOp(Op* op) {
   switch (op->type) {
   case Expr::FN_CALL: {
     auto* call = g<VarRef>(op->left);
     auto* args = g<FnCallArgs>(op->right);
-    if (call->name == "printf") { printf("Printf: '%s'\n", g<StrLit>(args->args[0])->val.c_str()); }
+    if (call->name == "printf") {
+      if (args->args.empty()) error(op, "printf requires at least 1 argument");
+      boost::format fmt = boost::format(g<StrLit>(args->args[0])->val);
+      for (int i = 1; i < int(args->args.size()); ++i)
+        fmt = fmt % getVar(args->args[i].get(), g<VarRef>(args->args[i])->name)->int_val;
+      printf("%s", fmt.str().c_str());
+    }
     break;
   }
   default: break;
   }
+}
+
+void Interpreter::error(Node* n, const std::string& msg) const {
+  verify_expr(false, "error at '%s' (%d:%d): %s", cts_->getSpan(n->tok.loc, n->tok.size).c_str(),
+      cts_->getLineNumber(n->tok.loc), cts_->getColNumber(n->tok.loc), msg.c_str());
+}
+
+Interpreter::Var* Interpreter::getVar(Node* n, const std::string& name) const {
+  auto iter = sym_.find(name);
+  if (iter == sym_.end()) error(n, "undeclared variable " + name);
+  return iter->second.get();
 }
 
 }  // namespace memelang
