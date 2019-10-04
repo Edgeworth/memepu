d8917d1f17dad8d97255ef5d7290ab33edcde606
E <edgeworth.dev@gmail.com>
2019-10-04 17:18:50 +0900

    Implement VM.

--- verymeme/include/memelang/constants.h
+++ verymeme/include/memelang/constants.h
@@ -2,6 +2,7 @@
 #ifndef MEMELANG_CONSTANTS_H
 #define MEMELANG_CONSTANTS_H
 
+#include <map>
 #include <string>
 
 namespace memelang {
@@ -19,6 +20,8 @@ inline const char* F32 = "f32";
 inline const char* F64 = "f64";
 
 inline const char* BUILTIN_TYPES[] = {BOOL, I8, I16, I32, I64, U8, U16, U32, U64, F32, F64};
+const std::map<std::string, int> BUILTIN_SIZE = {{BOOL, 1}, {I8, 1}, {I16, 2}, {I32, 4}, {I64, 8},
+    {U8, 1}, {U16, 2}, {U32, 4}, {U64, 8}, {F32, 4}, {F64, 8}};
 
 }  // namespace memelang
 
---
+++ verymeme/include/memelang/exec.h
@@ -0,0 +1,151 @@
+#ifndef MEMELANG_EXEC_H
+#define MEMELANG_EXEC_H
+
+#include <map>
+#include <set>
+
+#include "memelang/ast.h"
+#include "memelang/scopes.h"
+#include "memelang/vm.h"
+#include "verymeme/util.h"
+
+namespace memelang::exec {
+
+class Exec {
+public:
+  Exec(ast::File* file, const FileContents* cts);
+
+  void run();
+
+  void error(const std::string& msg) const;
+
+private:
+  ast::File* f_;
+  const FileContents* cts_;
+  ast::Node* node_ctx_{};
+
+  ScopeManager scope_;
+  VM vm_;
+  std::map<const ast::Type*, const Type*> ast_type_map_;
+  std::set<Type> types_;
+  std::map<Typename, ast::Fn*> fns_;
+  std::map<Typename, ast::Enum*> enums_;
+  std::map<Typename, ast::Intf*> intfs_;
+  std::map<Typename, ast::Struct*> structs_;
+  // Map from a type (may be set) to mapping from interface to the impl.
+  std::map<const Type*, std::map<const Type*, ast::Impl*>> impls_;
+
+  // Built-in types
+  const Type* bool_{};
+  const Type* i8_{};
+  const Type* i16_{};
+  const Type* i32_{};
+  const Type* i64_{};
+  const Type* u8_{};
+  const Type* u16_{};
+  const Type* u32_{};
+  const Type* u64_{};
+  const Type* f32_{};
+  const Type* f64_{};
+
+  void setContext(ast::Node* node);
+
+  Val runFn(ast::Fn* fn, const std::vector<Val>& args);
+  Val runStmtBlk(ast::StmtBlk* blk);
+  Val runStmt(ast::Node* stmt);
+  void runVarDefn(ast::VarDefn* defn);
+  Val runVarDecl(ast::VarDecl* decl);
+  Val runFor(ast::For* fr);
+  Val runWhile(ast::While* wh);
+  Val runIf(ast::If* ifst);
+  Val runOp(ast::Op* op);
+
+  Val eval(ast::Node* n);
+  ast::Fn* getFn(const Typename& tname, ast::Node* n);
+
+  const Type* addType(Type&& t);
+  Val valFromAstType(ast::Type* type);
+  const Type* typeFromAst(ast::Type* ast_type);
+  Typename typenameFromAst(ast::Typename* ast_typename);
+
+  // Value operations:
+  Val assign(Val l, Val r);
+  Val add(Val l, Val r);
+  Val sub(Val l, Val r);
+  Val lt(Val l, Val r);
+  Val lor(Val l, Val r);
+  Val eq(Val l, Val r);
+  Val neq(Val l, Val r);
+  Val array_access(Val l, Val r);
+  Val preinc(Val l);
+  Val postinc(Val l);
+  Val addr(const Val& l);
+  Val copy(Val dst, Val src);
+  Val deref(const Val& l);
+
+  template <typename F>
+  auto invokeBuiltin(Val v, F op) {
+    if (v.type == bool_) return std::invoke(op, vm_.ref<bool>(v));
+    else if (v.type == i8_)
+      return std::invoke(op, vm_.ref<int8_t>(v));
+    else if (v.type == i16_)
+      return std::invoke(op, vm_.ref<int16_t>(v));
+    else if (v.type == i32_)
+      return std::invoke(op, vm_.ref<int32_t>(v));
+    else if (v.type == i64_)
+      return std::invoke(op, vm_.ref<int64_t>(v));
+    else if (v.type == u8_)
+      return std::invoke(op, vm_.ref<uint8_t>(v));
+    else if (v.type == u16_)
+      return std::invoke(op, vm_.ref<uint16_t>(v));
+    else if (v.type == u32_)
+      return std::invoke(op, vm_.ref<uint32_t>(v));
+    else if (v.type == u64_)
+      return std::invoke(op, vm_.ref<uint64_t>(v));
+    else if (v.type == f32_)
+      return std::invoke(op, vm_.ref<float>(v));
+    else if (v.type == f64_)
+      return std::invoke(op, vm_.ref<double>(v));
+    error("not builtin");
+    return std::invoke(op, vm_.ref<int8_t>(v));
+  }
+
+  template <typename F>
+  Val binop(Val l, Val r, const Type* type, const std::string& op_name, F default_op) {
+    // Look in impls for l's type
+    // TODO: Generalise look-up procedure, define rules for lookup.
+    auto impl_iter = impls_.find(l.type);
+    if (impl_iter != impls_.end()) {
+      for (const auto& [impl_type, impl] : impl_iter->second) {
+        if (impl_type->name != "Comparable") continue;
+        if (impl->tintf->params.size() != 1) continue;
+        // TODO: better lookup rules
+        if (typeFromAst(impl->tintf->params[0].get()) != r.type) continue;
+        for (const auto& fn : impl->fns) {
+          if (fn->sig->tname->name == op_name) { return runFn(fn.get(), {r}); }
+        }
+      }
+    }
+    if (l.type != r.type) error("no Comparable defined and types don't match");
+
+    return invokeBuiltin(l, [this, &default_op, &r, &type](auto lt) {
+      auto res = default_op(lt, vm_.ref<decltype(lt)>(r));
+      Val v{.hnd = vm_.allocTmp(sizeof(res)), .type = type};
+      vm_.ref<decltype(res)>(v) = res;
+      return v;
+    });
+  }
+
+  template <typename F>
+  Val unop(Val l, const Type* type, F default_op) {
+    // TODO: Impl operator overloading?
+    auto res = invokeBuiltin(l, [this, &default_op](auto lt) { return default_op(lt); });
+    Val v{.hnd = vm_.allocTmp(sizeof(res)), .type = type};
+    vm_.ref<decltype(res)>(v) = res;
+    return v;
+  }
+};
+
+}  // namespace memelang::exec
+
+#endif  // MEMELANG_EXEC_H
