5688b1d9defe133982ea5485a0c20f54ed4051d4
E <edgeworth.dev@gmail.com>
2020-01-01 10:58:20 +0900

    Reorganise test data.

--- verymeme/src/tests/memecad_tests.cpp
+++ verymeme/src/tests/memecad_tests.cpp
@@ -15,33 +15,35 @@ std::string removeWhitespace(const std::string& input) {
 using SchematicTest = testing::Test;
 
 TEST_F(SchematicTest, Parsing0) {
-  std::string full_adder_original = readFile("test_data/parsing0/full_adder.sch");
+  std::string full_adder_original = readFile("test_data/memecad/full_adder.sch");
   memecad::Sheet sheet = memecad::parseSheet(full_adder_original);
   std::string full_adder_output = memecad::writeSheet(sheet);
   EXPECT_EQ(removeWhitespace(full_adder_original), full_adder_output);
 }
 
 TEST_F(SchematicTest, Hierarchical0) {
-  auto files = memecad::convertVerilogToKicadSchematics("test_data/common/memecad_map.json",
-      {"test_data/hierarchical0/tapb_top_level.v", "test_data/hierarchical0/two_a_plus_b.v",
-          "test_data/hierarchical0/full_adder.v", "test_data/common/chip7408.v",
-          "test_data/common/chip7432.v", "test_data/common/chip7486.v"},
-      {"test_data/common/74xx.lib"});
+  auto files = memecad::convertVerilogToKicadSchematics("test_data/memecad/memecad_map.json",
+      {"test_data/memecad/hierarchical/tapb_top_level.v",
+          "test_data/memecad/hierarchical/two_a_plus_b.v",
+          "test_data/memecad/hierarchical/full_adder.v", "test_data/memecad/chip7408.v",
+          "test_data/memecad/chip7432.v", "test_data/memecad/chip7486.v"},
+      {"test_data/memecad/74xx.lib"});
   EXPECT_EQ(3u, files.size());
   for (const auto& file : files) {
-    std::string golden = readFile("test_data/hierarchical0/output/" + file.filename);
+    std::string golden = readFile("test_data/memecad/hierarchical/output/" + file.filename);
     EXPECT_EQ(golden, file.contents);
   }
 }
 
 TEST_F(SchematicTest, Alu0) {
-  auto files = memecad::convertVerilogToKicadSchematics("test_data/common/memecad_map.json",
-      {"test_data/alu0/alu.v", "test_data/alu0/alu_lookahead.v", "test_data/alu0/sram17x8.v",
-          "test_data/alu0/alu_slice.v", "test_data/alu0/sram.v", "test_data/common/chip7408.v"},
-      {"test_data/common/74xx.lib", "test_data/common/Memory_RAM.lib"});
+  auto files = memecad::convertVerilogToKicadSchematics("test_data/memecad/memecad_map.json",
+      {"test_data/memecad/alu/alu.v", "test_data/memecad/alu/alu_lookahead.v",
+          "test_data/memecad/alu/sram17x8.v", "test_data/memecad/alu/alu_slice.v",
+          "test_data/memecad/alu/sram.v", "test_data/memecad/chip7408.v"},
+      {"test_data/memecad/74xx.lib", "test_data/memecad/Memory_RAM.lib"});
   EXPECT_EQ(3u, files.size());
   for (const auto& file : files) {
-    std::string golden = readFile("test_data/alu0/output/" + file.filename);
+    std::string golden = readFile("test_data/memecad/alu/output/" + file.filename);
     EXPECT_EQ(golden, file.contents);
   }
 }
